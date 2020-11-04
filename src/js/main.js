bc82c23e9789c5ee99ffedf3a84e3853f396a5e2
Artur Petriv <artur.petriv@gmail.com>
2020-11-05 00:37:21 +0200

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
+import Swiper from "swiper/bundle"
 
+document.addEventListener(`DOMContentLoaded`, function () {
+    const mySwiper = new Swiper(`.swiper-container`, {
+        loop: true,
+        simulateTouch: false,
+        pagination: {
+            el: `.swiper-pagination`,
+            clickable: true,
+        },
+        navigation: {
+            nextEl: `.swiper-button-next`,
+            prevEl: `.swiper-button-prev`,
+        },
+    })
+})
