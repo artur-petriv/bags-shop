f5b27755f3004d052a7a95afa6edf543b4aab1b7
Artur Petriv <artur.petriv@gmail.com>
2020-11-05 12:50:58 +0200

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,12 @@ document.addEventListener(`DOMContentLoaded`, function () {
     const mySwiper = new Swiper(`.swiper-container`, {
         loop: true,
         simulateTouch: false,
+        flipEffect: {
+            slideShadows: false,
+        },
+        autoplay: {
+            delay: 8000,
+        },
         pagination: {
             el: `.swiper-pagination`,
             clickable: true,
